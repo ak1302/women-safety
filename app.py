bc028d9fbee1a903b63d81a0a8772e0b9eaebc7b
Akshita <akshitababurajan13@gmail.com>
2022-04-25 02:42:19 +0530

    new app

--- app.py
+++ app.py
@@ -42,15 +42,16 @@ def sms_ahoy_reply():
     # Start our response
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+
     # chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     
         # selenium webdriver path in your system...
-    # service = Service(executable_path=ChromeDriverManager().install())
+    service = Service(executable_path=ChromeDriverManager().install())
 
     # driver = webdriver.Chrome(service=service)
 
