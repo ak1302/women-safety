7b4bb94cc7b8fbd11b1b7ea1d3f878388fddea8e
Akshita <akshitababurajan13@gmail.com>
2022-04-25 01:45:12 +0530

    new

--- app.py
+++ app.py
@@ -20,179 +20,179 @@ from sklearn.tree import DecisionTreeRegressor
 from selenium.webdriver.common.keys import Keys
 
 
-# app = Flask(__name__)
-# app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
-# @app.route('/')
-# def index():
-    # return "Hello world"
+@app.route('/')
+def index():
+    return "Hello world"
 
 
-# # @app.route("/sms", methods=['GET', 'POST'])
-# def sms_ahoy_reply():
-#     """Respond to incoming messages with a friendly SMS."""
+# @app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
 
-#     # body = request.values.get('Body', None)
+    # body = request.values.get('Body', None)
     # src = request.form.get('src')
     # dest = request.form.get('des')
-# src = "iit dhanbad"
-# dest = "dhanbad junction"
+    src = "iit dhanbad"
+    dest = "dhanbad junction"
 
-#     # Start our response
+    # Start our response
 
     
         # selenium webdriver path in your system...
-# service = Service(executable_path=ChromeDriverManager().install())
+    service = Service(executable_path=ChromeDriverManager().install())
 
-# driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service)
 
-# driver.get('https://www.google.com/maps/dir///@27.9107022,78.0760799,15z/data=!4m2!4m1!3e0')
+    driver.get('https://www.google.com/maps/dir///@27.9107022,78.0760799,15z/data=!4m2!4m1!3e0')
 
-# driver.set_window_size(1024,600)
-# driver.maximize_window()
+    driver.set_window_size(1024,600)
+    driver.maximize_window()
 
-# time.sleep(5)
-
-
-# dataset1 = pd.read_csv('crime.csv')
-# X = dataset1.drop('Crime value', axis=1)
-# y = dataset1['Crime value']
-
-# #from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# #from sklearn.tree import DecisionTreeRegressor
-# regressor = DecisionTreeRegressor()
-# regressor.fit(X_train, y_train)
-
-# driver.find_element(By.XPATH,'//*[@id="sb_ifc50"]/input').send_keys(src)
-# driver.find_element(By.XPATH, '//*[@id="sb_ifc51"]/input').send_keys(dest)
-# driver.find_element(By.XPATH, '//*[@id="sb_ifc51"]/input').send_keys(Keys.RETURN)
-
-# time.sleep(5)
+    time.sleep(5)
 
 
+    dataset1 = pd.read_csv('crime.csv')
+    X = dataset1.drop('Crime value', axis=1)
+    y = dataset1['Crime value']
 
-# road_xpath = '//h1[@id="section-directions-trip-title-0" or @id="section-directions-trip-title-1" or@id="section-directions-trip-title-2"]/span'
-# roads = driver.find_elements(By.XPATH, road_xpath)
-# print(roads)
+    #from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# list_of_via_roads_final1 = [None,]
-# list_of_via_roads_final2 = []
+    #from sklearn.tree import DecisionTreeRegressor
+    regressor = DecisionTreeRegressor()
+    regressor.fit(X_train, y_train)
 
+    driver.find_element(By.XPATH,'//*[@id="sb_ifc50"]/input').send_keys(src)
+    driver.find_element(By.XPATH, '//*[@id="sb_ifc51"]/input').send_keys(dest)
+    driver.find_element(By.XPATH, '//*[@id="sb_ifc51"]/input').send_keys(Keys.RETURN)
 
-# for road in roads:
-#     road_name = road.text
-#     print(road_name)
-#     list_of_via_roads_final2.append(road_name)
-#     check_for_and = "and" in road_name
-#     check_for_slash = "/" in road_name
+    time.sleep(5)
 
 
 
-#     if check_for_and == True:
+    road_xpath = '//h1[@id="section-directions-trip-title-0" or @id="section-directions-trip-title-1" or@id="section-directions-trip-title-2"]/span'
+    roads = driver.find_elements(By.XPATH, road_xpath)
+    print(roads)
 
-#         road_name_final1 = road_name.split('and')[0].strip()
-#         road_name_final2 = road_name.split('and')[1].strip()
-#         list_of_via_roads_final1.append(road_name_final1)
-#         for items in range(len(list_of_via_roads_final1)):
-#             if list_of_via_roads_final1[items] == road_name_final1:
-#                 list_of_via_roads_final1.append(road_name_final2)
-
-#             else:
-#                 list_of_via_roads_final1.append(road_name_final1)
+    list_of_via_roads_final1 = [None,]
+    list_of_via_roads_final2 = []
 
 
-#     elif check_for_slash == True:
-
-#         road_name_final1 = road_name.split('/')[0].strip()
-#         list_of_via_roads_final1.append(road_name_final1)
-
-#     else:
-#         road_name_final1 = road_name
-#         list_of_via_roads_final1.append(road_name_final1)
-
-# print(road_name)
-# # pickle.dump(road,open('model.pkl','wb'))
+    for road in roads:
+        road_name = road.text
+        print(road_name)
+        list_of_via_roads_final2.append(road_name)
+        check_for_and = "and" in road_name
+        check_for_slash = "/" in road_name
 
 
 
-# def remove_duplicates(values):
-#     output = []
-#     seen = set()
-#     for value in values:
-#         # If value has not been encountered yet,
-#         # ... add it to both list and set.
-#         if value not in seen:
-#             output.append(value)
-#             seen.add(value)
-#     return output
+        if check_for_and == True:
 
-# # Remove duplicates from this list.
-# values = list_of_via_roads_final1
-# result = remove_duplicates(values)
+            road_name_final1 = road_name.split('and')[0].strip()
+            road_name_final2 = road_name.split('and')[1].strip()
+            list_of_via_roads_final1.append(road_name_final1)
+            for items in range(len(list_of_via_roads_final1)):
+                if list_of_via_roads_final1[items] == road_name_final1:
+                    list_of_via_roads_final1.append(road_name_final2)
 
-# list_of_via_roads_final1 = result
+                else:
+                    list_of_via_roads_final1.append(road_name_final1)
 
 
+        elif check_for_slash == True:
 
-# clean = [x for x in list_of_via_roads_final1 if x != None]
-# print(clean)
+            road_name_final1 = road_name.split('/')[0].strip()
+            list_of_via_roads_final1.append(road_name_final1)
 
-# b = 0
-# c = 0
-# A = [None] * 5
-# for i in range(len(clean)):
-#     # name_of_road = list_of_via_roads_final1[i]
-#     csv_file = csv.reader(open('roads.csv', 'r', encoding='utf-8'))
-#     # print(name_of_road)
+        else:
+            road_name_final1 = road_name
+            list_of_via_roads_final1.append(road_name_final1)
 
-#     for row in csv_file:
-#         if clean[i] == row[0]:
-# #             print(row)
-#             row.pop(0)
-#             #print(row)
-#             #print(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
-#             A[i] = regressor.predict([[row[0], row[1], row[2], row[3]]])
-
-# #             print(A[i])
-#             b = b + 1
-
-# m = A[0]
-# # print(A)
-# for i in range(b):
-#     if int(m)> int(A[i]):
-#         m = A[i]
-#         c = i
-
-
-# # print(m)
-# # print(c)
-
-
-# time.sleep(1)
-# clicking_path = f'//div[@id="section-directions-trip-{c}"]'
-
-# driver.find_element(By.XPATH, clicking_path).click()
-# try:
-#     driver.find_element(By.XPATH, clicking_path).click()
-# except:
-#     pass
-
-# print("Safest route will be via " + list_of_via_roads_final2[c])
-# final_result = "Safest route will be via " + list_of_via_roads_final2[c]
-
-
-# time.sleep(5)
-
-# url_current = driver.current_url
-# print(url_current)
-#     return jsonify({'safest route': url_current})
+    print(road_name)
+    # pickle.dump(road,open('model.pkl','wb'))
 
 
 
-# if __name__ == "__main__":
-#     app.run(debug='true')
+    def remove_duplicates(values):
+        output = []
+        seen = set()
+        for value in values:
+            # If value has not been encountered yet,
+            # ... add it to both list and set.
+            if value not in seen:
+                output.append(value)
+                seen.add(value)
+        return output
+
+    # Remove duplicates from this list.
+    values = list_of_via_roads_final1
+    result = remove_duplicates(values)
+
+    list_of_via_roads_final1 = result
+
+
+
+    clean = [x for x in list_of_via_roads_final1 if x != None]
+    print(clean)
+
+    b = 0
+    c = 0
+    A = [None] * 5
+    for i in range(len(clean)):
+        # name_of_road = list_of_via_roads_final1[i]
+        csv_file = csv.reader(open('roads.csv', 'r', encoding='utf-8'))
+        # print(name_of_road)
+
+        for row in csv_file:
+            if clean[i] == row[0]:
+    #             print(row)
+                row.pop(0)
+                #print(row)
+                #print(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
+                A[i] = regressor.predict([[row[0], row[1], row[2], row[3]]])
+
+    #             print(A[i])
+                b = b + 1
+
+    m = A[0]
+    # print(A)
+    for i in range(b):
+        if int(m)> int(A[i]):
+            m = A[i]
+            c = i
+
+
+    # print(m)
+    # print(c)
+
+
+    time.sleep(1)
+    clicking_path = f'//div[@id="section-directions-trip-{c}"]'
+
+    driver.find_element(By.XPATH, clicking_path).click()
+    try:
+        driver.find_element(By.XPATH, clicking_path).click()
+    except:
+        pass
+
+    print("Safest route will be via " + list_of_via_roads_final2[c])
+    final_result = "Safest route will be via " + list_of_via_roads_final2[c]
+
+
+    time.sleep(5)
+
+    url_current = driver.current_url
+    print(url_current)
+    return jsonify({'safest route': url_current})
+
+
+
+if __name__ == "__main__":
+    app.run(debug='true')
 
 # import speech_recognition as speech
 # import pickle
@@ -460,197 +460,3 @@ from selenium.webdriver.common.keys import Keys
 
 
 
-
-from flask import Flask, request, jsonify
-
-import time
-# from selenium import webdriver
-import numpy as np
-
-# from selenium import  webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.common.by import By
-import csv
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
-# from selenium.webdriver.common.keys import Keys
-
-
-app = Flask(_name_)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
-
-@app.route('/')
-# def index():
-    # return "Hello world"
-
-
-# @app.route("/sms", methods=['GET', 'POST'])
-def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
-
-    # body = request.values.get('Body', None)
-    # src = request.form.get('src')
-    # dest = request.form.get('des')
-    src = "iit dhanbad"
-    dest = "dhanbad junction"
-
-    # Start our response
-
-
-    #     # selenium webdriver path in your system...
-    # service = Service(executable_path=ChromeDriverManager().install())
-
-    # driver = webdriver.Chrome(service=service)
-
-    # # driver.get('https://www.google.com/maps/dir///@27.9107022,78.0760799,15z/data=!4m2!4m1!3e0')
-
-    # driver.set_window_size(1024,600)
-    # driver.maximize_window()
-
-    time.sleep(5)
-
-
-    dataset1 = pd.read_csv('crime.csv')
-    X = dataset1.drop('Crime value', axis=1)
-    y = dataset1['Crime value']
-
-    #from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-    #from sklearn.tree import DecisionTreeRegressor
-    regressor = DecisionTreeRegressor()
-    regressor.fit(X_train, y_train)
-
-    # driver.find_element(By.XPATH,'//*[@id="sb_ifc50"]/input').send_keys(src)
-    # driver.find_element(By.XPATH, '//*[@id="sb_ifc51"]/input').send_keys(dest)
-    # driver.find_element(By.XPATH, '//*[@id="sb_ifc51"]/input').send_keys(Keys.RETURN)
-
-    time.sleep(5)
-
-
-    roads=[]
-    # road_xpath = '//h1[@id="section-directions-trip-title-0" or @id="section-directions-trip-title-1" or@id="section-directions-trip-title-2"]/span'
-    roads.append("Dr B R Ambedkar Marg/Gaondevi Marg")
-    roads.append("Jagatguru Aadi Shankracharya Marg and Dr B R Ambedkar Marg/Gaondevi Marg")
-    roads.append("Jagatguru Aadi Shankracharya Marg")
-    print(roads)
-
-    list_of_via_roads_final1 = [None,]
-    list_of_via_roads_final2 = []
-
-
-    for road in roads:
-        road_name = road
-        print(road)
-        print(road_name)
-        list_of_via_roads_final2.append(road_name)
-        check_for_and = "and" in road_name
-        check_for_slash = "/" in road_name
-
-
-
-        if check_for_and == True:
-
-            road_name_final1 = road_name.split('and')[0].strip()
-            road_name_final2 = road_name.split('and')[1].strip()
-            list_of_via_roads_final1.append(road_name_final1)
-            for items in range(len(list_of_via_roads_final1)):
-                if list_of_via_roads_final1[items] == road_name_final1:
-                    list_of_via_roads_final1.append(road_name_final2)
-
-                else:
-                    list_of_via_roads_final1.append(road_name_final1)
-
-
-        elif check_for_slash == True:
-
-            road_name_final1 = road_name.split('/')[0].strip()
-            list_of_via_roads_final1.append(road_name_final1)
-
-        else:
-            road_name_final1 = road_name
-            list_of_via_roads_final1.append(road_name_final1)
-
-    print(road_name)
-    # pickle.dump(road,open('model.pkl','wb'))
-
-
-
-    def remove_duplicates(values):
-        output = []
-        seen = set()
-        for value in values:
-            # If value has not been encountered yet,
-            # ... add it to both list and set.
-            if value not in seen:
-                output.append(value)
-                seen.add(value)
-        return output
-
-    # Remove duplicates from this list.
-    values = list_of_via_roads_final1
-    result = remove_duplicates(values)
-
-    list_of_via_roads_final1 = result
-
-
-
-    clean = [x for x in list_of_via_roads_final1 if x != None]
-    print(clean)
-
-    b = 0
-    c = 0
-    A = [None] * 5
-    for i in range(len(clean)):
-        # name_of_road = list_of_via_roads_final1[i]
-        csv_file = csv.reader(open('roads.csv', 'r', encoding='utf-8'))
-        # print(name_of_road)
-
-        for row in csv_file:
-            if clean[i] == row[0]:
-    #             print(row)
-                row.pop(0)
-                #print(row)
-                #print(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
-                A[i] = regressor.predict([[row[0], row[1], row[2], row[3]]])
-
-    #             print(A[i])
-                b = b + 1
-
-    m = A[0]
-    # print(A)
-    for i in range(b):
-       if int(m)> int(A[i]):
-           m = A[i]
-           c = i
-
-
-    # print(m)
-    # print(c)
-
-
-    time.sleep(1)
-    # clicking_path = f'//div[@id="section-directions-trip-{c}"]'
-
-    # driver.find_element(By.XPATH, clicking_path).click()
-    # try:
-    #     driver.find_element(By.XPATH, clicking_path).click()
-    # except:
-    #     pass
-
-    print("Safest route will be via " + list_of_via_roads_final2[c])
-    final_result = "Safest route will be via " + list_of_via_roads_final2[c]
-
-
-    time.sleep(5)
-
-    # url_current = driver.current_url
-    
-    return jsonify({'safest route': final_result})
-
-
-
-if _name_ == "_main_":
-    app.run(debug='true')
